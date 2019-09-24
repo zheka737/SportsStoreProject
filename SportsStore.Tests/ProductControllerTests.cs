68f4ab4e9288505305264a37536a9d139465bbf6
zheka737 <zheka737@gmail.com>
2019-09-24 13:30:29 +0300

    Нест

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -15,7 +15,7 @@ namespace SportsStore.Tests{
 
             mock.Setup(m => m.Products).Returns((new Product[] {
                 new Product {ProductID = 1, Name = "P1"},
-                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 2, Name = "P2"},111
                 new Product {ProductID = 3, Name = "P3"},
                 new Product {ProductID = 4, Name = "P4"},
                 new Product {ProductID = 5, Name = "P5"}
