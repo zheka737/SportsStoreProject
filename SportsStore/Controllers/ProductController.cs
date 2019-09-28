959e8a109390e6cbf0af72e5d40d6183bcfbdbdd
zheka737 <zheka737@gmail.com>
2019-09-28 15:30:55 +0300

    Pagination 2

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -14,10 +15,17 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int productPage = 1) 
-            => View(repository.Products.OrderBy(e => e.ProductID)
-            .Skip((productPage - 1) * PageSize)
-            .Take(PageSize));
-
+        public ViewResult List(int productPage = 1)
+            => View(new ProductsListViewModel {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
