840108c8e4381a7505e1a86006bc951aea2306f2
Феникс Женя <zheka737@gmail.com>
2019-10-24 17:33:08 +0300

    Фикс

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent: ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService){
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke() {
+            return View(cart);
+        }
+    }
+}
